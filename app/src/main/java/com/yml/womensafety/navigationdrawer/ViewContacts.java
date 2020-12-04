03a64568243e2390eba6b5c461c22d4cfe0f8878
Deeksha K <deeksha1399@gmail.com>
2020-12-04 11:25:16 +0530

    Changes to view contacts xml

---
+++ app/src/main/java/com/yml/womensafety/navigationdrawer/ViewContacts.java
@@ -0,0 +1,16 @@
+package com.yml.womensafety.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yml.womensafety.R;
+
+public class ViewContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contacts);
+    }
+}

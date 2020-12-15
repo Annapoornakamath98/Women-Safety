67c17e01b2abcfeb1c440c3360622bc6b6f2494d
Annapoornakamath98 <annapoornakamathc@gmail.com>
2020-12-15 17:57:09 +0530

    Formatted the code.

--- app/src/main/java/com/yml/womensafety/FirebaseUtil.kt
+++ app/src/main/java/com/yml/womensafety/FirebaseUtil.kt
@@ -4,11 +4,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 object FirebaseUtil {
-     var firebaseDatabase: FirebaseDatabase ?= null
-     var user: FirebaseAuth ?= null
+    var firebaseDatabase: FirebaseDatabase? = null
+    var user: FirebaseAuth? = null
     fun initializeFirebase() {
         user = FirebaseAuth.getInstance()
     }
+
     fun initializeDatabase() {
         firebaseDatabase = FirebaseDatabase.getInstance()
     }

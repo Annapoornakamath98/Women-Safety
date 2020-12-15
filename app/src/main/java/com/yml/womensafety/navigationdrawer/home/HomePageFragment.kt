afabde7a6d01f990cd77a607e4941238674727b2
Annapoornakamath98 <annapoornakamathc@gmail.com>
2020-12-15 15:41:14 +0530

    Introduced repository layer to display user details

--- app/src/main/java/com/yml/womensafety/navigationdrawer/home/HomePageFragment.kt
+++ app/src/main/java/com/yml/womensafety/navigationdrawer/home/HomePageFragment.kt
@@ -2,38 +2,26 @@ package com.yml.womensafety.navigationdrawer.home
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.yml.womensafety.FirebaseUtil
 import com.yml.womensafety.R
+import com.yml.womensafety.Response
+import com.yml.womensafety.viewModel.FirebaseViewModel
 import kotlinx.android.synthetic.main.fragment_home_page.*
 
 class HomePageFragment : Fragment(R.layout.fragment_home_page) {
-    private var databaseReference: DatabaseReference? = null
+    lateinit var firebaseViewModel: FirebaseViewModel
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val appUser = FirebaseUtil.user?.currentUser
-        databaseReference =
-            FirebaseUtil.firebaseDatabase?.reference?.child(getString(R.string.name_column))
-        val userReference = databaseReference?.child(appUser?.uid!!)
-        userReference?.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val userFullName =
-                    snapshot.child(getString(R.string.full_name_column)).value.toString()
-                val userName = getString(R.string.hi_user) + userFullName
-                tvLabel.text = userName
+        firebaseViewModel = ViewModelProvider(this).get(FirebaseViewModel()::class.java)
+        firebaseViewModel.initializeRepository()
+        firebaseViewModel.getUserName(object : Response {
+            override fun onUserDetailReceiveSuccess(userDetails: String) {
+                tvLabel.text = getString(R.string.hi_user) + userDetails
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(context, error.message, Toast.LENGTH_LONG).show()
-            }
         })
-
         cvEscapeThreat.setOnClickListener {
             view.findNavController().navigate(R.id.action_homePageFragment_to_escapeThreatFragment)
         }

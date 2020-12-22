e8a567651278028ec56523751c39bcc15c255c00
Annapoornakamath98 <annapoornakamathc@gmail.com>
2020-12-22 20:51:11 +0530

    Addressed the comments.

--- app/src/main/java/com/yml/womensafety/Alert.kt
+++ app/src/main/java/com/yml/womensafety/Alert.kt
@@ -1,11 +1,17 @@
 package com.yml.womensafety
 
-import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
 
+/**
+ * This is used to display an Alert Dialog
+ * @param context context of the activity class
+ * @param title the title of alert dialog
+ * @param message the message to be displayed in alert dialog
+ * @param positiveMessage the message of the positive button in alert dialog
+ */
 object Alert {
-    fun Activity.alert(
+    fun alert(
         context: Context,
         title: CharSequence,
         message: CharSequence,
@@ -15,6 +21,7 @@ object Alert {
             .setTitle(title)
             .setMessage(message)
             .setPositiveButton(positiveMessage) { dialog, which ->
+                //Nothing to be implemented here
             }
 
         val dialog: AlertDialog = builder.create()
